7def5eff18847be34c0dc92ea9e844c492b19960
quang120901 <nguyennhatquang963@gmail.com>
2025-07-16 16:48:58 +0700

    update mark

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,10 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import todo_icon from "../assets/todo_icon.png"
 import TodoItems from "./TodoItems";
 
 const Todo = () => {
 
-const [todoList, setTodoList] = useState([]);
+const [todoList, setTodoList] = useState(localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : []);
 const inputRef = useRef();
 
 const add = () => {
@@ -24,22 +24,36 @@ const add = () => {
 }
 
 const deleteTodo = (id) => {
-    setTodoList((prvTodos)=>{
-        return prvTodos.filter((todo)=> todo.id !== id)
+    setTodoList((prevTodos)=>{
+        return prevTodos.filter((todo)=> todo.id !== id)
     })
 }
+
+const toggle = (id) => {
+    setTodoList((prevTodos)=>{
+        return prevTodos.map((todo)=>{
+            if(todo.id === id) {
+                return {...todo, isComplete: !todo.isComplete}
+            }
+            return todo;
+        })
+    })
+}
+
+useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todoList))
+}, [todoList])
 
     return (
         <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
 
            {/* -----------------title ------------------- */}
-            <div className="flex items-center mt-7 gap-2">
+            <div className="flex items-center mt-7 gap-2 justify-center">
                 <img className="w-8" src={todo_icon} alt="" />
-                <h1 className="text-3xl font-semibold">To-do list</h1>
+                <h1 className="text-3xl font-semibold center">To-do list</h1>
             </div>
 
             {/* -----------------input box ------------------- */}
-
             <div className="flex items-center my-7 bg-gray-200 rounded-full">
                 <input ref={inputRef} className="bg-transparent border-0 outline-none flex-1 h-14 pl-16 pr-2 placeholder:text-slate-600" 
                 type="text" placeholder="Add your task"/>               
@@ -48,10 +62,9 @@ const deleteTodo = (id) => {
             </div>
 
              {/* -----------------todo list ------------------- */}
-
             <div>
                 {todoList.map((item, index)=>{
-                    return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo}/>
+                    return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle}/>
                 })}
             </div>
 
