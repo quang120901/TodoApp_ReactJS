a8c45ab84999c3adc2136d01434c05a11224af44
quang120901 <nguyennhatquang963@gmail.com>
2025-07-16 17:01:05 +0700

    update vite file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'TodoApp_ReactJS',
+  base:'/TodoApp_ReactJS',
   plugins: [react(),
     tailwindcss(),
   ],
